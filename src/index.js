5350f8dbfa66d543d3365aab970ea6741c8ba891
Shafiaks <113788543+Shafiaks@users.noreply.github.com>
2022-11-17 18:54:25 +0100

    changed app,js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserReact} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserReact>
+    <BrowserRouter>
     <App />
-    </BrowserReact>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
